459ff6dddcfb3aef72349b3c031767f0f2ebe41f
Fabian Castro <facaslo.99@gmail.com>
2023-03-07 19:55:45 -0500

    Terrain generator updated

--- content/sketches/terrain.js
+++ content/sketches/terrain.js
@@ -1,18 +1,21 @@
 let cols, rows;
 let scale = 20;
-let w = 1200;
-let h = 1000;
+let w =  1000;
+let h = 600;
+let lowest = -100;
+let highest = 100;
 
 let flying = 0;
 
 function setup() {
-  createCanvas(800, 600, WEBGL);
+  createCanvas(600, 600, WEBGL);
   cols = w / scale;
   rows = h / scale;  
   
 }
 
-function draw() {  
+function draw() {    
+  frameRate(30)
   let terrainElevation = [] ;
   flying -= 0.1;
   let yoff = flying;
@@ -20,7 +23,7 @@ function draw() {
     let terrainRow = [];
     let xoff = 0;
     for (let col = 0; col <= cols; col++) {
-      terrainRow.push(map(noise(xoff,yoff),0,1,-100,100)); 
+      terrainRow.push(map(noise(xoff,yoff),0,1, lowest , highest)); 
       xoff += 0.2;
     }    
     terrainElevation.push(terrainRow);
@@ -29,13 +32,18 @@ function draw() {
   
   background(0);
   // Move the origin to the top-left corner of the canvas
-  rotateX(PI/3);
-  translate(-w/2, -h/2, 0);
+  rotateX(PI/3.5);
+  translate(-w/2, -h/2, 0);  
+
   for (let row = 0; row < rows; row++) {
     beginShape(TRIANGLE_STRIP);
-    for (let col = 0; col <= cols; col++) {
-      vertex(col*scale,row*scale, terrainElevation[row][col] );
-      vertex(col*scale,(row+1)*scale, terrainElevation[row+1][col]);
+    noStroke();
+    // stroke(color(44,250,31));
+    for (let col = 0; col <= cols; col++) {   
+      strokeColor = color(map(terrainElevation[row][col], lowest , highest , 0 , 255));
+      fill(strokeColor)            
+      vertex(col*scale,row*scale, terrainElevation[row][col]);
+      vertex(col*scale,(row+1)*scale, terrainElevation[row+1][col] );      
     }
     endShape();
   }
